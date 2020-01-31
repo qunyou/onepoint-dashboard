fa4dbf794fd6c0a595ea6768042822aa6cc47226
游逸群 <qun@youyiqundeiMac.local>
2020-01-31 14:44:07 +0800

    AuthController

--- src/Publishes/app/Http/Controllers/AuthController.php
+++ src/Publishes/app/Http/Controllers/AuthController.php
@@ -26,14 +26,14 @@ class AuthController extends Controller
     /**
      * 重導判斷
      */
-    public function index(PathPresenter $path_presenter)
-    {
-        if (auth()->check()) {
-            return redirect(config('dashboard.uri') . '/' . config('dashboard.login_default_uri', 'dashboard/index'));
-        } else {
-            return redirect(config('dashboard.uri') . '/auth/login');
-        }
-    }
+    // public function index(PathPresenter $path_presenter)
+    // {
+    //     if (auth()->check()) {
+    //         return redirect(config('dashboard.uri') . '/' . config('dashboard.login_default_uri', 'dashboard/index'));
+    //     } else {
+    //         return redirect(config('dashboard.uri') . '/auth/login');
+    //     }
+    // }
 
     /**
      * 登入頁
