8f4eacfd244b5f9b4f9de5d8842baee4d31eff77
Qun <qun@yi.idv.tw>
2020-02-20 12:00:29 +0800

    view update

--- src/resources/views/value.blade.php
+++ src/resources/views/value.blade.php
@@ -1,22 +1,22 @@
 @component('dashboard::components.inputs', $input_array = $formPresenter->setValue($input_setting, 'value'))
     {!! $input_array['prepend_str'] !!}
-    @if ($input_array['value_type'] == 'image')
-        {!! $input_array['input_value'] !!}
+    @if ($input_array['input_value'] === '啟用' || $input_array['input_value'] === '停用')
+        {{ __('backend.' . $input_array['input_value']) }}
     @else
-        @if ($input_array['input_value'] === '啟用' || $input_array['input_value'] === '停用')
-            {{ __('backend.' . $input_array['input_value']) }}
-        @else
-            @switch($input_array['value_type'])
-                @case('boolean')
-                    {{ $input_array['input_value'] ? __('backend.是') : __('backend.否') }}
-                    @break
-                @case('integer')
-                    {{ number_format($input_array['input_value'], $input_array['parameter']['decimals'] ?? 0) }}
-                    @break
-                @default
-                    {{ $input_array['input_value'] }}
-            @endswitch
-        @endif
+        @switch($input_array['value_type'])
+            @case('image')
+            @case('file')
+                {!! $input_array['input_value'] !!}
+                @break
+            @case('boolean')
+                {{ $input_array['input_value'] ? __('backend.是') : __('backend.否') }}
+                @break
+            @case('integer')
+                {{ number_format($input_array['input_value'], $input_array['parameter']['decimals'] ?? 0) }}
+                @break
+            @default
+                {{ $input_array['input_value'] }}
+        @endswitch
     @endif
     {!! $input_array['depend_str'] !!}
 @endcomponent
