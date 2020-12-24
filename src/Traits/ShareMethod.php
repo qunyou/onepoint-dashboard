5f1cceeec0fdc5520f1d33925c94763d2cb9574d
Qun <qun@yi.idv.tw>
2020-12-24 21:33:30 +0800

    增加語言設定註解

--- src/Traits/ShareMethod.php
+++ src/Traits/ShareMethod.php
@@ -7,11 +7,14 @@ trait ShareMethod
     public function share()
     {
         // 設定語言
+        // 如果有問題，執行清除動作
+        // cache()->flush();
+        // cache()->forget('backend_language');
         if (request('lang', false)) {
             cache()->forever('backend_language', request('lang'));
         }
         \App::setLocale(cache('backend_language', 'zh-tw'));
-
+        
         // 判斷是否使用分站網址
         $this->backend_url_suffix = '';
         if (config('backend_url_suffix', false)) {
