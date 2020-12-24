173ddbf5576649483e89dd6f3d4073f0fcfdfd31
Qun <qun@yi.idv.tw>
2020-12-24 21:19:50 +0800

    語言設定移至src/Traits/ShareMethod.php

--- src/Traits/ShareMethod.php
+++ src/Traits/ShareMethod.php
@@ -6,6 +6,12 @@ trait ShareMethod
 {
     public function share()
     {
+        // 設定語言
+        if (request('lang', false)) {
+            cache()->forever('backend_language', request('lang'));
+        }
+        \App::setLocale(cache('backend_language', 'zh-tw'));
+
         // 判斷是否使用分站網址
         $this->backend_url_suffix = '';
         if (config('backend_url_suffix', false)) {
