cade169f10c17e71fc965f89b81ff693e8c4fb1a
Qun <qun@yi.idv.tw>
2021-02-01 08:34:03 +0800

    密碼欄位修改

--- src/resources/views/password.blade.php
+++ src/resources/views/password.blade.php
@@ -1,5 +1,5 @@
 @component('dashboard::components.inputs', $input_array = $formPresenter->setValue($input_setting, 'text'))
-    <input type="password" class="form-control {{ $input_array['input_size_class'] }} @error($input_array['input_name']) is-invalid @enderror" id="{{ $input_array['input_name'] }}" name="{{ $input_array['input_name'] }}" value="{{ $input_array['input_value'] }}" {!! $input_array['attribute'] !!} placeholder="************">
+    <input type="password" class="form-control {{ $input_array['input_size_class'] }} @error($input_array['input_name']) is-invalid @enderror" id="{{ $input_array['input_name'] }}" name="{{ $input_array['input_name'] }}" value="{{ $input_array['input_value'] }}" {!! $input_array['attribute'] !!}>
 
     @error($input_array['input_name'])
         <div class="alert alert-danger mt-1">{{ $message }}</div>
