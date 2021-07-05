9d63ef3ed0896db68871638fe3c092344ceaaa76
Qun <qun@yi.idv.tw>
2021-07-06 07:23:44 +0800

    add time.blade

---
+++ src/resources/views/time.blade.php
@@ -0,0 +1,11 @@
+@if (!isset($formPresenter))
+    @inject('formPresenter', 'Onepoint\Dashboard\Presenters\FormPresenter')
+@endif
+
+@component('dashboard::components.inputs', $input_array = $formPresenter->setValue($input_setting))
+    <input type="time" class="form-control {{ $input_array['input_size_class'] }}  @error($input_array['input_name']) is-invalid @enderror" id="{{ $input_array['input_name'] }}" name="{{ $input_array['input_name'] }}" value="{{ $input_array['input_value'] }}" {!! $input_array['attribute'] !!}>
+
+    @error($input_array['input_name'])
+        <div class="alert alert-danger mt-1">{{ $message }}</div>
+    @enderror
+@endcomponent
