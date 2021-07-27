e8ce869836dc67f3177227be1c0ac6e8b3e52d3c
Qun <qun@yi.idv.tw>
2021-07-27 19:01:38 +0800

    翻譯檔修改

--- src/resources/views/components/backend-update-card.blade.php
+++ src/resources/views/components/backend-update-card.blade.php
@@ -2,21 +2,17 @@
     @csrf
     @method('PUT')
     <div class="card-update">
-        @if (isset($page_title))
-            <div class="row justify-content-between px-3">
+        <div class="row justify-content-between px-3">
+            @if (isset($page_title))
                 <div class="col-12">
                     <div class="card-title">{{ $page_title }}</div>
                 </div>
-                <div class="col-md-12 top-btn-group">
-                    {{-- @if ($back_url)
-                        <a class="btn btn-outline-deep-purple waves-effect d-xs-block" href="{{ $back_url }}">
-                            <i class="fa fa-fw fa-arrow-left"></i>@lang('dashboard::backend.回列表')
-                        </a>
-                    @endif --}}
-                    {{ $top_btn ?? '' }}
-                </div>
+            @endif
+            <div class="col-md-12 top-btn-group">
+                {{ $top_btn ?? '' }}
             </div>
-        @endif
+        </div>
+
         {{ $slot }}
         @if (isset($footer_hide) ? !$footer_hide : true)
             <footer>
