4c7cb8580298e1f813a95f50de31ce4dd090598c
Qun <qun@yi.idv.tw>
2021-12-14 09:55:48 +0800

    number.blade

--- src/resources/views/number.blade.php
+++ src/resources/views/number.blade.php
@@ -17,4 +17,12 @@
         }
     @endphp
     <input type="number" class="form-control{{ $input_array['input_size_class'] }}" id="{{ $input_array['input_name'] }}" name="{{ $input_array['input_name'] }}" value="{{ $input_value }}" {!! $input_array['attribute'] !!}{{-- step="any" --}}>
+    @if($input_array['required'])
+        <span class="input-group-text">@lang('dashboard::backend.必填')</span>
+    @endif
+    @error($input_array['input_name'])
+        @slot('error_slot')
+            <div class="alert alert-danger mt-1 form-text">{{ $message }}</div>
+        @endslot
+    @enderror
 @endcomponent
